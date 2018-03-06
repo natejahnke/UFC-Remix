9786500f1336b9eb471c88887e1b79f6972cc375
Jahnke <nathan.jahnke@tsi.corp.com>
2018-03-06 15:26:37 -0600

    added a second api call for images of fighter search

--- server.js
+++ server.js
@@ -31,19 +31,24 @@ app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
 });
 
-// var search = "Max Holloway";
-// search = search.replace(/\s/g, "-");
-// var url = "http://www.ufc.com/fighter/" + search
-// ufc.getFighter(url, function(data) {
-//     console.log("Name: " + data.fullname + "\nAge: " + data.age + "\nHeight: " + data.height + "\nWeight: " + data.weight + "\nRecord: " + data.record);
-// });
+var firstName = "Nate";
+var lastName = "Diaz";
+
+var search = firstName + " " + lastName;
+search = search.replace(/\s/g, "-");
+console.log(search);
+var url = "http://www.ufc.com/fighter/" + search
+ufc.getFighter(url, function(data) {
+    console.log("Name: " + data.fullname + "\nAge: " + data.age + "\nHeight: " + data.height + "\nWeight: " + data.weight + "\nRecord: " + data.record);
+});
+
 
 // Then run a request to the OMDB API with the movie specified
 request("http://ufc-data-api.ufc.com/api/v3/us/fighters", function(error, response, body) {
 
     // If the request is successful (i.e. if the response status code is 200)
     if (!error && response.statusCode === 200) {
-        const obj = JSON.parse(body).find(o => o.last_name === 'Jones' & o.first_name === 'Jon');
+        const obj = JSON.parse(body).find(o => o.last_name === lastName & o.first_name === firstName);
         // Parse the body of the site and recover just the imdbRating
         // (Note: The syntax below for parsing isn't obvious. Just spend a few moments dissecting it).
         // console.log(JSON.parse(body)[0]);
@@ -51,9 +56,3 @@ request("http://ufc-data-api.ufc.com/api/v3/us/fighters", function(error, respon
 
     }
 });
-
-
-// var url = "http://www.fightmatrix.com/fighter-profile/Jon+Jones/"
-// fightMatrix.getFighter(url, function(data) {
-//     console.log(data);
-// });
